c13572783f0cf53ae97f323b62810f8dcb64ab4a
MMSG97 <maria.m.mga88@gmail.com>
2024-06-18 16:05:59 +0200

    mise à jour des avancées sur github pour 2eme pc et mise en place d'add.Listenere et de conditions oour les fleches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,80 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// const bannerImg = document.querySelectorAll(".banner-img");
+// const left = document.querySelector(".arrow_left")
+// const right = document.querySelector(".arrow_right")
+
+// console.log(bannerImg)
+// console.log(left)
+// console.log(right)
+// // let slideContent = [
+// 	"images/slideshow/slide1.png",
+// 	"images/slideshow/slide2.png",
+// 	"images/slideshow/slide3.png",
+// 	"images/slideshow/slide4.png",
+// ]
+// console.log(slideContent);
+
+//  valeurs img
+let index = 0
+console.log(index);
+
+console.log(slideContent.length);
+let lastimg = slideContent.length -1
+let startimg = 0
+
+// valeur imgs html
+let img = document.querySelector(".first");
+console.log(img);
+
+// let imgs = document.querySelectorAll(".banner-img");
+// console.log(imgs);
+
+// valeurs flèches
+let left = document.querySelector(".arrow_left");
+console.log(left);
+
+let right = document.querySelector(".arrow_right");
+console.log(right);
+
+// valeur .banner p
+let tagLine = document.querySelector("#banner p");
+console.log(tagLine);
+
+console
+// EventListener flèche gauche
+left.addEventListener("click", function() {
+if(index === startimg){
+	index = lastimg
+}
+else {
+	index = index - 1
+	// ou   index --
+}
+img.setAttribute("src", slideContent[index])
+// tagLine.innerHTML = index
+})
+
+// EventListener flèche droite
+right.addEventListener("click", function() {
+	if(index === lastimg){
+		index = startimg
+	}
+	else {
+		index = index + 1
+		// ou   index ++
+	}
+	img.setAttribute("src", slideContent[index])
+	// tagLine.innerHTML = index
+	})
+
+
+
+
+
+
+
+
+
