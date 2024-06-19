203897ffc067950289c534b6a03cb70dc4423d3d
MMSG97 <mariaintweb@gmail.com>
2024-06-19 04:41:44 +0200

    ajout des fonctions 'prevClick' et 'nextClick', ajout des EventListener des flèches. Et modif de la 1ère ligne de else pour chaque fonction avec '--' et '++'

--- assets/script.js
+++ assets/script.js
@@ -16,86 +16,150 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-console.log(slides)
+// console.log(slides)
+const slideSize = slides.length;
+// console.log(slideSize)
 
-//  valeurs img
-let image = document.querySelector(".banner-img");
-console.log(image);
-let index = 0
-console.log(index);
+//  slide actuelle
+let selectedSlide = 0
+// console.log(selectedSlide);
 
-// valeurs flèches
-let leftArrow = document.querySelector(".arrow_left");
-console.log(leftArrow);
+//initialisation banner
+let img = document.querySelector(".banner-img");
+img.setAttribute("src", "./assets/./assets/images/slideshow/" + slides[selectedSlide]["image"]);
 
-let rightArrow = document.querySelector(".arrow_right");
-console.log(rightArrow);
+// console.log(img);
 
-// valeur .banner p
+// initialisation .banner p
 let tagLine = document.getElementById("tagLine");
-console.log(tagLine);
+tagLine.innerHTML = slides[selectedSlide]["tagLine"];
 
-console.log(slides.length);
-let lastImg = slides.length -1
-let firstImg = 0
-console.log(lastImg)
-console.log(firstImg)
-
-// image.setAttribute("src", slides[index])
-// tagLine.innerHTML = index
-
-// EventListener flèche gauche
-leftArrow.addEventListener("click", function() {
-if(index === firstImg){
-	index = lastImg
-}
-else {
-	index = index - 1
-	// ou   index --
-}
-img.setAttribute("src", slides[index])
-// tagLine.innerHTML = index
-})
-
-// EventListener flèche droite
-rightArrow.addEventListener("click", function() {
-if(index === lastImg){
-	index = firstImg
-}
-else {
-	index = index + 1
-	// ou   index ++
-}
-img.setAttribute("src", slides[index])
-// tagLine.innerHTML = index
-})
+// console.log(tagLine);
 
 //boucle + création des .dots &__selected
-let slidesize = slides.length;
-console.log(slidesize)
-
-for (let i = 0; i < slidesize; i++) {
+for (let i = 0; i < slideSize; i++) {
 	if (i === 0) {
-		//dot selected
+		//selected dot
 		let dot = document.createElement("div");
 		dot.setAttribute("class", "dot dot_selected");
-		// dot.setAttribute("id", i);
+		dot.setAttribute("id", i);
 		let dotContainer = document.querySelector(".dots");
 		dotContainer.appendChild(dot);
 	}else{
-		//dots not selected
+		//not selected dots
 		let dot = document.createElement("div");
 		dot.setAttribute("class", "dot");
-		// dot.setAttribute("id", i);
-		let dotContainer = document.querySelector("dots");
+		dot.setAttribute("id", i);
+		let dotContainer = document.querySelector(".dots");
 		dotContainer.appendChild(dot);
 	}
 	
 }
 
+// function & EventListener flèche gauche
+const leftArrow = document.querySelector(".arrow_left");
+console.log(leftArrow);
+
+function prevClick() {
+	if (selectedSlide === 0) {
+		selectedSlide = slideSize - 1;
+		img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+		tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+		dot = document.getElementById(selectedSlide);
+		dot.classList.add("dot_selected");
+		NotSelected = document.getElementById(0);
+		NotSelected.classList.remove("dot_selected");
+	} else {
+		selectedSlide--;
+		img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+		tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+		dot = document.getElementById(selectedSlide);
+		dot.classList.add("dot_selected");
+		NotSelected = document.getElementById(selectedSlide + 1);
+		NotSelected.classList.remove("dot_selected");
+	}
+}
+leftArrow.addEventListener("click", prevClick);
+
+// Function & EventListener flèche droite
+const rightArrow = document.querySelector(".arrow_right");
+console.log(rightArrow);
+
+function nextClick() {
+	if (selectedSlide === slideSize - 1) {
+		selectedSlide = 0;
+		img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+		tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+		dot = document.getElementById(selectedSlide);
+		dot.classList.add("dot_selected");
+		NotSelected = document.getElementById(slideSize - 1);
+		NotSelected.classList.remove("dot_selected");
+	}else {
+		selectedSlide++;
+		img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+		tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+		dot = document.getElementById(selectedSlide);
+		dot.classList.add("dot_selected");
+		NotSelected = document.getElementById(selectedSlide - 1);
+		NotSelected.classList.remove("dot_selected");
+	}
+}
+rightArrow.addEventListener("click", nextClick)
 
 
 
+// let lastImg = slides.length -1
+// let firstImg = 0
 
 
+// img.setAttribute("src", slides[selectedSlide])
+// tagLine.innerHTML = selectedSlide
 
+// leftArrow.addEventListener("click", function(){
+// if(selectedSlide === firstImg){
+// 	selectedSlide = lastImg
+// 	img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+// 	tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+// 	dot = document.getElementById(selectedSlide);
+// 	dot.classList.add("dot_selected");
+// 	NotSelected = document.getElementById(0);
+// 	NotSelected.classList.remove("dot_selected");
+// }
+// else {
+// 	selectedSlide = selectedSlide - 1
+// 	// ou   selectedSlide --
+// 	img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+// 	tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+// 	dot = document.getElementById(selectedSlide);
+// 	dot.classList.add("dot_selected");
+// 	NotSelected = document.getElementById(selectedSlide + 1);
+// 	NotSelected.classList.remove("dot_selected");
+// }
+// img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+// tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+// })
+
+
+// rightArrow.addEventListener("click", function(){
+// if(selectedSlide === lastImg){
+// 	selectedSlide = firstImg;
+// 	// img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+// 	// tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+// 	dot = document.getElementById(selectedSlide);
+// 	dot.classList.add("dot_selected");
+// 	NotSelected = document.getElementById(slideSize - 1);
+// 	NotSelected.classList.remove("dot_selected");
+// }
+// else {
+// 	selectedSlide = selectedSlide + 1
+// 	// ou   selectedSlide ++
+// 	// img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+// 	// tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+// 	dot = document.getElementById(selectedSlide);
+// 	dot.classList.add("dot_selected");
+// 	NotSelected = document.getElementById(selectedSlide - 1);
+// 	NotSelected.classList.remove("dot_selected");
+// }
+// img.setAttribute("src", "./assets/images/slideshow/" + slides[selectedSlide]["image"]);
+// tagLine.innerHTML = slides[selectedSlide]["tagLine"];
+// })
